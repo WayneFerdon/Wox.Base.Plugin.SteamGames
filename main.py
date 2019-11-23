4c93c90dabe961793f48666d8e02f686615085ff
wayneferdon <wayneferdon@hotmail.com>
2019-11-24 01:37:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ class appInfoDecoder:
         self.offset = 0  # Parsing offset
 
         # Commonly used structs
-        self._readInt32 = self.makeCustomReader('<I', singleValue=True)
-        self._readInt64 = self.makeCustomReader('<Q', singleValue=True)
+        self._readInt32_ = self.makeCustomReader('<I', singleValue=True)
+        self._readInt64_ = self.makeCustomReader('<Q', singleValue=True)
         self.readVdfHeader = self.makeCustomReader('<2I')
         self.readAppHeader = self.makeCustomReader('<3IQ20sI')
 
@@ -63,7 +63,7 @@ class appInfoDecoder:
         # Parsing applications
         appFieldList = ('size', 'state', 'last_update', 'access_token', 'checksum', 'change_number')
         while True:
-            appId = self._readInt32()
+            appId = self._readInt32_()
             # AppID = 0 marks the last application in the App info
             if appId == 0:
                 break
@@ -110,7 +110,7 @@ class appInfoDecoder:
                 break
 
             key = self.readString()
-            value = self.valueParserDic.get(valueType, self._unknownValueType)()
+            value = self.valueParserDic.get(valueType, self._unknownValueType_)()
 
             subsection[key] = value
 
@@ -135,11 +135,11 @@ class appInfoDecoder:
             return returnMany
 
     def readInt32(self):
-        number = self._readInt32()
+        number = self._readInt32_()
         return Integer(data=number, size=32)
 
     def readInt64(self):
-        number = self._readInt64()
+        number = self._readInt64_()
         return Integer(data=number, size=64)
 
     def readByte(self):
@@ -160,83 +160,100 @@ class appInfoDecoder:
             return self.data[string].tobytes()
 
     @staticmethod
-    def _unknownValueType():
+    def _unknownValueType_():
         raise ValueError(
             'Cannot parse the provided data type.'
         )
 
 
-class steamLauncher(Wox):
-    # set icon database paths
-    iconDatabase = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps'
+class steamLocal:
+    def __init__(self):
+        sysPathList = os.environ['path'].split(';')
+        for sysPath in sysPathList:
+            if os.path.isfile(sysPath + '/steam.exe'):
+                self.steamPath = sysPath
+                break
 
-    # find the first steam path from system environment
-    sysPathList = os.environ['path'].split(';')
-    for sysPath in sysPathList:
-        if os.path.isfile(sysPath + '/steam.exe'):
-            steamPath = sysPath
-            break
+    def __localAppInfo__(self):
+        with open(self.steamPath + '/appCache/appInfo.vdf', 'rb') as appInfoVdf:
+            appInfo = appInfoDecoder(appInfoVdf.read(), wrapper=dict).decode(self.__localAppId__()).items()
+        return appInfo
 
-    # load libList from steam installation path
-    with open(steamPath + '/steamApps/libraryFolders.vdf') as libFoldersVdf:
-        libList = vdf.load(libFoldersVdf)['LibraryFolders']
+    def __localLib__(self):
+        with open(self.steamPath + '/steamApps/libraryFolders.vdf') as libFoldersVdf:
+            libList = vdf.load(libFoldersVdf)['LibraryFolders']
+        return libList
 
-    # load apps' id from steam library
-    appIdList = []
-    libNum = 1
-    libDirKey = str(libNum)
-    while libDirKey in libList:
-        libDir = libList[libDirKey] + '/steamApps'
-        for file in os.scandir(libDir):
-            if 'appManifest'.lower() in file.name and '228980' not in file.name:
-                appIdList.append(
-                    file.name.replace('appManifest_'.lower(), '').replace('.acf', '')
-                )
-        libNum += 1
+    def __localAppId__(self):
+        libList = self.__localLib__()
+        appIdList = list()
+        libNum = 1
         libDirKey = str(libNum)
+        while libDirKey in libList.keys():
+            libDir = libList[libDirKey] + '/steamApps'
+            for file in os.scandir(libDir):
+                if (
+                        'appManifest'.lower() in file.name
+                        and '228980' not in file.name
+                ):
+                    appIdList.append(
+                        file.name.replace('appManifest_'.lower(), '').replace('.acf', '')
+                    )
+            libNum += 1
+            libDirKey = str(libNum)
+        return appIdList
 
-    # get apps' title, icon
-    # load app info vdf for loading client icon id
-    with open(steamPath + '/appCache/appInfo.vdf', 'rb') as appInfoVdf:
-        appInfoList = appInfoDecoder(appInfoVdf.read(), wrapper=dict).decode(appIdList).items()
-    appList = []
-    for appInfo in appInfoList:
-        appId = appInfo[0]
-        detail = appInfo[1]['sections'][b'appInfo'.lower()][b'common']
-        appIconId = detail[b'clientIcon'.lower()].decode('utf-8')
-        appIcon = steamPath + '/steam/games/' + appIconId + '.ico'
-        if not os.path.isfile(appIcon):
-            try:
-                urlretrieve(url=iconDatabase + '/' + str(appId) + '/' + appIconId + '.ico', filename=appIcon)
-            except Exception:
-                appIcon = './Image/steamIcon.png'
-        appList.append(
-            {
-                'appId': appId,
-                'appTitle': detail[b'name'].decode('utf-8'),
-                'appIcon': appIcon
-            }
-        )
+    def appInfoList(self):
+        iconDatabase = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps'
+        appList = list()
+        for appInfo in self.__localAppInfo__():
+            appId = appInfo[0]
+            detail = appInfo[1]['sections'][b'appInfo'.lower()][b'common']
+            appIconId = detail[b'clientIcon'.lower()].decode('utf-8')
+            appIcon = self.steamPath + '/steam/games/' + appIconId + '.ico'
+            if not os.path.isfile(appIcon):
+                try:
+                    urlretrieve(url=iconDatabase + '/' + str(appId) + '/' + appIconId + '.ico', filename=appIcon)
+                except Exception:
+                    appIcon = './Image/steamIcon.png'
+            appList.append(
+                {
+                    'appId': appId,
+                    'appTitle': detail[b'name'].decode('utf-8'),
+                    'appIcon': appIcon
+                }
+            )
+        return appList
 
-    def query(self, queryString):
-        result = []
-        appList = self.appList
 
+class regexList:
+    def __init__(self, queryString):
         queryStringLower = queryString.lower()
         queryList = queryStringLower.split()
-        regexList = []
+        self.regexList = list()
         for query in queryList:
             # pattern = '.*?'.join(query)
             # regexList.append(re.compile(pattern))
-            regexList.append(re.compile(query))
+            self.regexList.append(re.compile(query))
 
+    def match(self, item):
+        match = True
+        for regex in self.regexList:
+            match = regex.search(item) and match
+        return match
+
+
+# class steamLauncher:
+class steamLauncher(Wox):
+    @classmethod
+    def query(cls, queryString):
+        appList = steamLocal().appInfoList()
+        result = list()
+        regex = regexList(queryString)
         for app in appList:
             appTitle = app['appTitle']
             appId = app['appId']
-            match = True
-            for regex in regexList:
-                match = regex.search(appTitle.lower() + str(appId)) and match
-            if match:
+            if regex.match(appTitle.lower() + str(appId)):
                 appIcon = app['appIcon']
                 result.append(
                     {
