c2c001bd21585ec3dbe4554d6231b26a37fb67e1
wayneferdon <wayneferdon@hotmail.com>
2022-10-05 18:43:48 +0800

    Optimizations

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Author: wayneferdon wayneferdon@hotmail.com
 # Date: 2022-02-12 06:25:55
 # LastEditors: wayneferdon wayneferdon@hotmail.com
-# LastEditTime: 2022-10-05 18:39:46
+# LastEditTime: 2022-10-05 18:41:31
 # FilePath: \Wox.Plugin.SteamGames\main.py
 # ----------------------------------------------------------------
 # Copyright (c) 2022 by Wayne Ferdon Studio. All rights reserved.
@@ -12,99 +12,16 @@
 # ----------------------------------------------------------------
 
 # -*- coding: utf-8 -*-
-import os
 import webbrowser
-from urllib.request import urlretrieve
-import vdf
-import winreg
 from WoxQuery import *
 from SteamInfoDecoder import *
 from RegexList import *
-
-class steamLocal:
-    def __init__(self):
-        regPath = r"steam\\Shell\\Open\\Command"
-        key = winreg.OpenKeyEx(winreg.HKEY_CLASSES_ROOT,regPath)
-        data = winreg.QueryValueEx(key,None)
-        self.steamPath =  str(data[0]).split("\"")[1].replace("\steam.exe","")
-
-    def __localAppInfo__(self):
-        with open(self.steamPath + '/appcache/appinfo.vdf', 'rb') as appInfoVdf:
-            appInfoList = SteamInfoDecoder(appInfoVdf.read(), wrapper=dict).decode(
-                self.__localAppId__()).items()
-        appInfoDict = dict()
-        for appInfo in appInfoList:
-            appId = appInfo[0]
-            appInfoDict[str(
-                appId)] = appInfo[1]['sections'][b'appInfo'.lower()][b'common']
-        return appInfoDict
-
-    def __localLib__(self):
-        with open(self.steamPath + '/steamApps/libraryFolders.vdf') as libFoldersVdf:
-            libList = vdf.load(libFoldersVdf)
-            libList = libList['libraryfolders']
-        return libList
-
-    def __localAppId__(self):
-        def scanApp(path):
-            for child in os.scandir(path):
-                if (
-                        'appManifest'.lower() in child.name
-                        and '228980' not in child.name
-                ):
-                    appIdList.append(
-                        child.name.replace(
-                            'appManifest_'.lower(), '').replace('.acf', '')
-                    )
-
-        libList = self.__localLib__()
-        appIdList = list()
-        scanApp(self.steamPath)
-        libNum = 1
-        libDirKey = str(libNum)
-        while libDirKey in libList.keys():
-            libDir = libList[libDirKey]['path'] + '/steamApps'
-            scanApp(libDir)
-            libNum += 1
-            libDirKey = str(libNum)
-        return appIdList
-
-    def appInfoList(self):
-        def getIcon(data):
-            if data[b'type'] != b'Music':
-                iconId = data[b'clientIcon'.lower()].decode('utf-8')
-                icon = self.steamPath + '/steam/games/' + iconId + '.ico'
-            else:
-                parentId = str(data[b'parent'].data)
-                parent = localAppInfo[parentId]
-                icon = getIcon(parent)
-            if not os.path.isfile(icon):
-                try:
-                    urlretrieve(url=iconDatabase + '/' + str(appId) +
-                                '/' + icon + '.ico', filename=icon)
-                except Exception:
-                    icon = './Image/steamIcon.png'
-            return icon
-
-        iconDatabase = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps'
-        appList = list()
-        localAppInfo = self.__localAppInfo__()
-        for appId in localAppInfo:
-            detail = localAppInfo[appId]
-            appIcon = getIcon(detail)
-            appList.append(
-                {
-                    'appId': appId,
-                    'appTitle': detail[b'name'].decode('utf-8'),
-                    'appIcon': appIcon
-                }
-            )
-        return appList
+from SteamLocal import *
 
 # class steamLauncher:
-class steamLauncher(WoxQuery):
+class SteamLauncher(WoxQuery):
     def query(self, queryString):
-        appList = steamLocal().appInfoList()
+        appList = SteamLocal().appInfoList()
         results = list()
         regex = RegexList(queryString)
         subTitle = 'Press Enter key to launch'
@@ -124,5 +41,5 @@ class steamLauncher(WoxQuery):
 
 
 if __name__ == '__main__':
-    steamLauncher()
+    SteamLauncher()
     # print(steamLauncher().query(""))
